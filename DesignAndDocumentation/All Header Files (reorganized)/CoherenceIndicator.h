d008a7623f4d3d60e60bb1aed47824fb9e5f4afb
raghadhamida <120336521+raghadhamida@users.noreply.github.com>
2023-04-23 23:02:14 -0400

    Update CoherenceIndicator.h

--- DesignAndDocumentation/All Header Files (reorganized)/CoherenceIndicator.h
+++ DesignAndDocumentation/All Header Files (reorganized)/CoherenceIndicator.h
@@ -1,8 +1,19 @@
-class CoherenceIndicator { 
+#ifndef COHERENCEINDICATOR_H
+#define COHERENCEINDICATOR_H
+
+#include <string>
+#include <QTimer>
 //this class indicates the coherence level of the user
-private:
-	int coherenceLevel;
+class CoherenceIndicator
+{
 public:
-	void setLevel();
-	void setLED(); //light that changes depending on coherence level
+    CoherenceIndicator(std::string ledWidget);
+    void setLevel(int level);
+    void setLED(); //light that changes depending on coherence level
+
+private:
+    int coherenceLevel;
+    std::string led;  //the LED light widget
 };
+
+#endif // COHERENCEINDICATOR_H
