83e9162e5d41ac69dc6fc25d71f040a4960f1978
raghadhamida <120336521+raghadhamida@users.noreply.github.com>
2023-04-23 22:53:03 -0400

    Add files via upload

---
+++ Qt C++ files/mainwindow.cpp
@@ -0,0 +1,49 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "user.h"
+#include "session.h"
+#include "history.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    currentTimerCount = -1;
+    powerStatus = false;
+    changePower();
+    connect(ui->powerButton, &QPushButton::released, this, &MainWindow::powerChange);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::changePower(){
+    ui->screen->setVisible(powerStatus);
+    ui->screenOff->setVisible(!powerStatus);
+    ui->HRcontact->setVisible(powerStatus);
+    ui->ach_Num->setVisible(powerStatus);
+    ui->achievement->setVisible(powerStatus);
+    ui->batteryLevel->setVisible(powerStatus);
+    ui->coh_Num->setVisible(powerStatus);
+    ui->coherence->setVisible(powerStatus);
+    ui->breathPacer->setVisible(powerStatus);
+    ui->len_Num->setVisible(powerStatus);
+    ui->length->setVisible(powerStatus);
+    ui->upButton->setEnabled(powerStatus);
+    ui->downButton->setEnabled(powerStatus);
+    ui->leftButton->setEnabled(powerStatus);
+    ui->rightButton->setEnabled(powerStatus);
+    ui->menuButton->setEnabled(powerStatus);
+    ui->okButton->setEnabled(powerStatus);
+    ui->backButton->setEnabled(powerStatus);
+    ui->powerButton->setEnabled(powerStatus);
+}
+
+void MainWindow::powerChange(){
+    powerStatus = !powerStatus;
+    changePower();
+}
