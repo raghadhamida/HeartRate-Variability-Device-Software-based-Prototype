a271b871977c18e37d996fa280432dff51790792
raghadhamida <120336521+raghadhamida@users.noreply.github.com>
2023-04-23 23:06:10 -0400

    Update User.h

--- DesignAndDocumentation/All Header Files (reorganized)/User.h
+++ DesignAndDocumentation/All Header Files (reorganized)/User.h
@@ -1,16 +1,24 @@
-class User { 
+#ifndef USER_H
+#define USER_H
+
 //this is the user interface
 //it has the suggested menu screen and on/off functions-
 //-as well as the menu and arrow button functionality from the project specs
-private:
-	bool isDeviceOn;
-	bool isSessionOn; 
-	string[] menuOptions;
+
+class User
+{
 public:
-	void on(); 
-	void off(); 
-	void displayMenu(); 
-	void displayGraph(); 
-	void selectMenuOption();
-	void selectArrowButton();
+    User();
+    void on();
+    void off();
+    void displayMenu();
+    void displayGraph();
+    void selectMenuOption();
+    void selectArrowButton();
+private:
+    bool isDeviceOn;
+    bool isSessionOn;
+    const char* menuOptions[3];
 };
+
+#endif // USER_H
