36bdbbf6857e07dfd57a9f8ba84236d6a0da1ca8
raghadhamida <120336521+raghadhamida@users.noreply.github.com>
2023-04-23 22:45:32 -0400

    Update CoherenceIndicator.cpp

--- Qt C++ files/CoherenceIndicator.cpp
+++ Qt C++ files/CoherenceIndicator.cpp
@@ -1,36 +1,32 @@
-class CoherenceIndicator
+#include "coherenceindicator.h"
+
+
+CoherenceIndicator::CoherenceIndicator(std::string ledWidget) : coherenceLevel(0), led(ledWidget)
 {
-private:
-    int coherenceLevel;
-    QLed *led;  //the LED light widget
-public:
-    CoherenceIndicator(QLed *ledWidget) : coherenceLevel(0), led(ledWidget)
+    //constructor initializes LED widget to off
+    led->setColor(Qt::black);
+    led->turnOff();
+}
+
+void CoherenceIndicator::setLevel(int level)
+{
+    coherenceLevel = level;
+    setLED();
+}
+
+void CoherenceIndicator::setLED() {
+    //set LED light based on coherence level
+    if (coherenceLevel <= 33)
     {
-        //constructor initializes LED widget to off
-        led->setColor(Qt::black);
-        led->turnOff();
+        led->setColor(Qt::red);
     }
-    
-    void setLevel(int level)
+    else if (coherenceLevel <= 66)
     {
-        coherenceLevel = level;
-        setLED();
+        led->setColor(Qt::blue);
     }
-    
-    void setLED() {
-        //set LED light based on coherence level
-        if (coherenceLevel <= 33)
-        {
-            led->setColor(Qt::red);
-        }
-        else if (coherenceLevel <= 66)
-        {
-            led->setColor(Qt::blue);
-        }
-        else
-        {
-            led->setColor(Qt::green);
-        }
-        led->turnOn();
+    else
+    {
+        led->setColor(Qt::green);
     }
-};
+    led->turnOn();
+}
