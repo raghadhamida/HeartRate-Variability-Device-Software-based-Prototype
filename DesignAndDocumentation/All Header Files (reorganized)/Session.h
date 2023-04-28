577dd94eab07e73bb6c19af3a60ee459ca959b49
KurlzTheGambler <103543795+KurlzTheGambler@users.noreply.github.com>
2023-04-28 19:19:58 -0400

    Update Session.h

--- DesignAndDocumentation/All Header Files (reorganized)/Session.h
+++ DesignAndDocumentation/All Header Files (reorganized)/Session.h
@@ -1,23 +1,22 @@
 #ifndef SESSION_H
 #define SESSION_H
 
+#include <QDebug>
 #include <QObject>
 #include <QTimer>
 
-//class for hadling each session's functionality and data
-class Session : public QObject
-{
+class Session : public QObject {
     Q_OBJECT
 
 public:
-    explicit Session(QObject *parent = nullptr);
+    explicit Session(QObject* parent = nullptr);
+    Session(const Session& other);
+    Session& operator=(const Session& other);
     void start();
     void end();
     void updateMetrics();
     void setInterval(int breathPacerInterval);
     ~Session();
-
-
 
 private:
     int metrics[3] = {0, 0, 0};
